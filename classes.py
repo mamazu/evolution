3041aa902d129dc52bd289289af989ac499ec4b9
mamazu <maxo.pech@gmail.com>
2017-03-10 23:27:27 +0100

    Implemented score calculations

--- classes.py
+++ classes.py
@@ -2,7 +2,16 @@ from random import randrange
 
 class Population:
 	def __init__(self, size=100):
-		self.pop = [Individuum() for _ in range(size)]
+		self.generation = 0
+		self.size = int(size)
+		self.pop = [Individuum() for _ in range(self.size)]
+
+	def evolve(self, aim):
+		for indi in self.pop:
+			indi.evaluate(aim)
+
+	def get_avg_score(self):
+		return sum([indi.score for indi in self.pop])//self.size
 
 class Individuum:
 	def __init__(self):
