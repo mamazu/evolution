014af311eca998a17a3d8408dd6be41a85ccdfe5
mamazu <maxo.pech@gmail.com>
2017-03-12 12:23:31 +0100

    Uses keyboard to end the programm instead of framecount

--- init.py
+++ init.py
@@ -16,17 +16,24 @@ print(population.get_avg_score())
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
-frame_count = 0
-while frame_count < 5000:
-	screen.fill((255,255,255))
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.KEYDOWN:
+			key = event.key
+			if key == pygame.K_ESCAPE:
+				running = False
+
+	screen.fill((255, 255, 255))
 	for indi in population.pop:
 		genes = indi.genes
 		position = genes.get_movement()
 		rel = (int(start[0]+position[0]), int(start[1]+position[1]))
-		pygame.draw.circle(screen, (255,0,0), rel, 2)
-	#Drawing start and end
-	pygame.draw.circle(screen, (0,0,255), start , 3)
-	rel_aim= (int(start[0]+aim[0]), int(start[1]+aim[1]))
-	pygame.draw.circle(screen, (0,255,0), rel_aim , 3)
+		pygame.draw.circle(screen, (255, 0, 0), rel, 2)
+	# Drawing start and end
+	pygame.draw.circle(screen, (0, 0, 255), start, 3)
+	rel_aim = (int(start[0]+aim[0]), int(start[1]+aim[1]))
+	pygame.draw.circle(screen, (0, 255, 0), rel_aim, 3)
 	pygame.display.update()
-	frame_count += 1
